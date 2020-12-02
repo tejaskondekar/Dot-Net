809e6ad630c2a69b5bce209b22ee3dbea7cd3491
tejaskondekar <65647121+tejaskondekar@users.noreply.github.com>
2020-12-02 13:12:47 +0530

    Add files via upload

---
+++ Assignment1-Updated/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e = new Employee();
+           Console.WriteLine ("Employee no =" + e.EmpNo);
+            Console.ReadLine();
+        }
+    }
+    class Employee
+    {
+        int empNo;
+        String Name;
+        decimal Basic;
+        short DeptNo;
+
+     public String name
+         {
+            set
+            {  
+                if (value = " ")
+                {
+                    Console.WriteLine("name can't be empty");
+                }
+            }
+            
+        }
+    }
+}
